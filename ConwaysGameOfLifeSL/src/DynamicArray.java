f4baa856db216e6b582a3b56017660e3737c78b5
haseganats <haseganats@gs161c.AD.ECS.VUW.AC.NZ>
2019-08-13 17:11:09 +1200

    update

--- ConwaysGameOfLifeSL/src/DynamicArray.java
+++ ConwaysGameOfLifeSL/src/DynamicArray.java
@@ -19,12 +19,17 @@ public class DynamicArray {
 
   public DynamicArray(){
 	   
-	  arrIni= new Cell[num][num];
+	  arrIni= new Cell[num+2][num+2];
 	  for (int i = 0; i < arrIni.length; i++) {
-      for (int j = 0; j < arrIni[i].length; j++) {
-        arrIni[i][j] = new Cell(i, j, true, false);
+      for (int j = 0; j < arrIni.length; j++) {
+        arrIni[i][j] = new Cell(i, j, false, false);
       }
     }
+	  for (int i = 1; i < arrIni.length-1; i++) {
+	      for (int j = 1; j < arrIni.length-1; j++) {
+	        arrIni[i][j] = new Cell(i, j, true, false);
+	      }
+	    }
    
   }
   
@@ -32,11 +37,11 @@ public class DynamicArray {
 	 new DynamicArray();
  }
  
-
-  
-
   public static Cell[][] extendsArray(Cell[][] array){
-    Cell[][] arr = new Cell[array.length*2][array[0].length*2];
+	  
+	  
+	
+    Cell[][] arr = new Cell[(array.length-2)*2][(array[0].length-2)*2];
     for(int i = 0; i<arr.length;i++){
       for(int j = 0; j<arr.length; j++){
         arr[i][j] = null;
@@ -56,18 +61,18 @@ public class DynamicArray {
   
   public void checkExpansion(Cell[][] arrIni) {
 	  
-	  for (int i = 0; i < arrIni.length; i++) {
+	  for (int i = 1; i < arrIni.length-1; i++) {
 
-	      if (arrIni[i][0].getCurrentStatus() == true || arrIni[0][i].getCurrentStatus() == true || arrIni[arrIni.length - 1][i].getCurrentStatus() == true || arrIni[i][arrIni.length - 1].getCurrentStatus() == true) {
+	      if (arrIni[i][1].getCurrentStatus() == true || arrIni[1][i].getCurrentStatus() == true || arrIni[arrIni.length - 2][i].getCurrentStatus() == true || arrIni[i][arrIni.length - 2].getCurrentStatus() == true) {
 	        Cell[][] temp = extendsArray(arrIni);
 	        arrIni = temp;
 	      }
 	    }
   }
   
-  public void updateStatus(Cell[][] arrIni) {
-	  for(int i=0;i<arrIni.length;i++) {
-		  for(int j=0;j<arrIni.length;j++) {
+  public void updateStatus() {
+	  for(int i=1;i<arrIni.length-1;i++) {
+		  for(int j=1;j<arrIni.length-1;j++) {
 			  Cell c = arrIni[i][j];
 			  int counter = checkNeighbor(c);
 			  if(c.getCurrentStatus()==true)
@@ -96,8 +101,8 @@ public class DynamicArray {
   {
 	Cell[][] tempNextGen = arr;  
   
-	  for(int i=0;i<tempNextGen.length;i++) {
-		  for(int j=0;j<tempNextGen.length;j++) {
+	  for(int i=1;i<tempNextGen.length-1;i++) {
+		  for(int j=1;j<tempNextGen.length-1;j++) {
 			  Cell c = tempNextGen[i][j];
 			  c.setCurrentStatus(c.getNextStatus());
 			  c.setNextStatus(false);
@@ -156,6 +161,17 @@ public class DynamicArray {
 	  return counter;
 
   }
+  
+  public void swapArr()
+  {
+	  arrIni = arrNext;
+  }
+  
+  public Cell[][] getArr()
+  {
+	  return arrIni;
+  }
+  
   
   
   }
