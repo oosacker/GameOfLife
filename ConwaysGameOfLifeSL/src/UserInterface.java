3952cf960629983751deb026a35ce3bf995cb66f
apple <apple@10.140.130.166>
2019-08-13 16:50:17 +1200

    updated

--- ConwaysGameOfLifeSL/src/UserInterface.java
+++ ConwaysGameOfLifeSL/src/UserInterface.java
@@ -1,4 +1,161 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TimelineBuilder;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class UserInterface {
-// test
+/**
+ * User interface class showing employing JavaFX and fetching data from backend. 
+ * 
+ * 13Aug19
+ * @author Lock, Samuel;
+ * @contact locksamu@myvuw.ac.nz;
+ * @create 13Aug19 
+ */
+
+
+
+//TODO Add favicon
+//TODO Timeline Builder
+
+
+
+public class UserInterface extends Application {
+
+	/**
+	 * Defines the speed of the starting animation
+	 */
+	private int worldSpeed = 16;
+	/**
+	 * Controls whether world is running or stopped.
+	 */
+	private boolean startStop = false;
+	
+	
+	
+	
+	public void start(Stage mainStage) throws Exception {
+		
+		// INITIAL ONSCREEN DISPLAY
+		Text welcomeText = new Text(); //TODO Do we need this for anything???!
+		welcomeText.setText("Click grid to set square");
+		HBox menuBox = new HBox();
+		createMenu(menuBox); 
+		
+		GridPane lifeGrid = new GridPane();
+		createGrid(lifeGrid);
+		
+		VBox bottomPane = new VBox(); // CreAteS MAIN VERTICAL BOX
+		bottomPane.getChildren().add(menuBox); // ADDS MENU TO MAIN VERTICAL BOX
+		bottomPane.getChildren().add(lifeGrid); // Adds lifegrid to bottompane beneath menu box
+		Scene mainScene = new Scene(bottomPane); // ADDS VBOX TO SCENE
+		mainStage.setWidth(700);
+		mainStage.setHeight(700);
+		
+		mainStage.setScene(mainScene); //ADDS ENTIRE SCENE TO MAINSTAGE
+		
+		// START MENU BUTTONS
+		Button startBtn = new Button("Start");
+		menuBox.getChildren().add(startBtn);
+		
+		//TODO TIMELINEBUILDER
+		// SETS SPEED OF ANIMATION
+		KeyFrame frame = new KeyFrame(Duration.millis(getWorldSpeed()), new EventHandler<ActionEvent>() {
+		
+			public void handle(ActionEvent event) {
+				
+				//TODO UPDATE 2D Array
+				//TODO CreateGrid(); // REDRAW GRID, How do you call from inside a hidden method?????
+				
+				
+			}
+		});
+		
+		Timeline tl = TimelineBuilder.create().cycleCount(javafx.animation.Animation.INDEFINITE).keyFrames(frame).build();
+		
+		startBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				
+				if (startStop == false) {
+					tl.play();
+					startStop = true;
+					System.out.println("Click Start");
+				} else if(startStop == true) {
+					tl.pause();
+					startStop = false;
+					System.out.println("Click Pause");
+				}
+			}
+			
+		});
+		
+		
+		
+		
+		mainStage.show(); // SHOWS MAINSTAGE ON SCREEN
+	}
+	
+	
+	
+
+	/**
+	 * Controls visual features of Menu Box, padding etc.
+	 * @param menuBox - passes HBox from inside stage.
+	 */
+	
+	public void createMenu(HBox menuBox) {
+		menuBox.setAlignment(Pos.CENTER);
+		menuBox.setSpacing(20);
+		
+		//TODO SET SPACING AND PADDING, BUTTON WIDTH
+	}
+	
+	/**
+	 * 
+	 * Creates the features of the gridpane that displays the Cells and their current position.
+	 * @param lifeGrid - passes the grid we are using to display living cells 
+	 */
+	public void createGrid(GridPane lifeGrid) {
+		//TODO Fetch Array and display items in grid at current position.
+		
+		
+	}
+
+
+	/**
+	 * Launches JavaFX
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	
+	/**
+	 * Returns animation speed for timelinebuilder
+	 * @return worldSpeed for Timeline Builder
+	 */
+	public int getWorldSpeed() {
+		return worldSpeed;
+	}
+	
+	/**
+	 * Sets animation speed for timelinebuilder
+	 * @param int new worldSpeed
+	 */
+	public void setWorldSpeed(int worldSpeed) {
+		this.worldSpeed = worldSpeed;
+	}
+
 }
